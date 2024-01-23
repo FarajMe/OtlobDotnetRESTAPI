f051375c88b5aae612804a6ae54b98ab4da77f2c
FarajMe <farajmakkawi08@gmail.com>
2024-01-23 18:13:59 +0200

    Order Properties

--- OtlobRESTAPI/Data/Models/Order.cs
+++ OtlobRESTAPI/Data/Models/Order.cs
@@ -2,6 +2,12 @@
 {
     public class Order
     {
-       //helooooooooo
+        public int Id { get; set; }
+        public int userId { get; set; }
+        public int restaurantId { get; set; }
+        public int statusId { get; set; }
+        public double totalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
